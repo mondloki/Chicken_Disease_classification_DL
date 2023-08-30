03c9d12c0285685f726b0ee84d701cf342fab734
mondloki <mondretysivakumar@gmail.com>
2023-08-30 22:58:39 +0530

    dvc pipeline created

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -105,8 +105,8 @@ class ConfigurationManager:
 
     def get_validation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
-            path_of_model=Path("artifacts/training/model.h5"),
-            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            path_of_model=Path(self.config.training.trained_model_path),
+            training_data=Path(os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")),
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE

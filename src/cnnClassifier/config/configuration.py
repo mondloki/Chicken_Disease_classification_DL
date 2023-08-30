595b059610e2f60f34ba53d4e83d841d53243221
mondloki <mondretysivakumar@gmail.com>
2023-08-30 21:36:19 +0530

    Model Training completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -71,7 +71,7 @@ class ConfigurationManager:
         prepare_callback_config = PrepareCallbacksConfig(
             root_dir=Path(config.root_dir),
             tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
-            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+            checkpoint_model_filepath=config.checkpoint_model_filepath
         )
 
         return prepare_callback_config
